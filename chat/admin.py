e061f6255f920ace9fe6f2062ea28cec5efe2c90
Tobias Ullrich <ullrich.tobias@gmx.de>
2023-01-04 12:18:40 +0100

    second commit

--- chat/admin.py
+++ chat/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import Chat, Message
+
+
+class MessageAdmin(admin.ModelAdmin):
+   fields = ('chat','text','created_at', 'author', 'receiver') #Diese Felder werden angezeigt
+   list_display = ('created_at', 'author', 'text', 'receiver') #Zeigt die Felder in Reihenfolge schön nebeneinander an
+   search_fields = ('text',) #Fertiger Filter für das Feld-Text
 
 # Register your models here.
+admin.site.register(Message, MessageAdmin)
+admin.site.register(Chat)
--- chat/views.py
+++ chat/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from .models import Chat, Message
 
 # Create your views here.
+def index(request):
+    if request.method == 'POST':
+     print ("Received data" + request.POST['textmessage']) #Gibt uns die Nachricht aus, falls es sich um eine POST-Methode handelt
+     myChat = Chat.objects.get(id=1) #Zieht uns den Chat mit der ID 1 und dieses Objekt wird unten mit der Message-Tabelle verknüpft
+     Message.objects.create(text=request.POST['textmessage'], chat=myChat, author=request.user, receiver=request.user) #Erstellt einen Datenbankeintrag und füllt die Felder
+     chatMessages = Message.objects.filter(chat__id=1) #Wir holen uns alle Nachrichten in eine Variable wo die Chat-ID = 1 ist
+    return render(request, 'chat/index.html', {'messages': chatMessages}) #Gibt uns etwas zurück
